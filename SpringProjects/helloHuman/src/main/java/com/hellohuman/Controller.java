b38e9d40ee361f378848b5e69c48d8096e8cdb7d
Brandon_Hart <sugb73@gmail.com>
2022-04-09 17:27:38 -0400

    Completed Assignment

---
+++ SpringProjects/helloHuman/src/main/java/com/hellohuman/Controller.java
@@ -0,0 +1,21 @@
+package com.hellohuman;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller 
+{
+	
+	@RequestMapping("/")
+	public String index(@RequestParam(value="q", required=false) String searchQuery)
+	{
+		if(searchQuery == null)
+		{
+			return "Hello Human!";
+		} else {
+		return "Hello " + searchQuery;
+		}
+	}
+}
