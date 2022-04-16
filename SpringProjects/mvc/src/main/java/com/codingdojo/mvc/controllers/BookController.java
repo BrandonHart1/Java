bb387bfbaa77fcf66fb71162510bbba4b0e11452
Brandon_Hart <sugb73@gmail.com>
2022-04-16 14:40:50 -0400

    Completed Assignment

---
+++ SpringProjects/mvc/src/main/java/com/codingdojo/mvc/controllers/BookController.java
@@ -0,0 +1,45 @@
+package com.codingdojo.mvc.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.codingdojo.mvc.models.Book;
+import com.codingdojo.mvc.services.BookService;
+
+@Controller
+public class BookController 
+{
+	@Autowired  // <<<---------- automatically does dependency injection ----------
+	BookService bookService;
+	
+	@GetMapping("/books/{bookId}")
+	public String index(Model model, @PathVariable("bookId") Long bookId)
+	{
+		System.out.println(bookId); // ----- verify book in the console -----
+		
+		Book book = bookService.findBook(bookId);
+		System.out.println(book); // ----- verify book id and object in the console -----
+		
+		model.addAttribute("book", book);
+		
+//		ArrayList<Book> books = (ArrayList<Book>) bookService.allBooks();
+//		
+//		model.addAttribute("books",books);
+		
+	
+		return "index.jsp";
+		
+	}
+	
+	@GetMapping("/books/3")
+	public String index()
+	{
+		return "show.jsp";
+	}
+	
+}
